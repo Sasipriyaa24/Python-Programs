0d4cebe28dac337d81b8d2d97c3c6e0df9c7dccd
Sasi Priyaa <sasipriyaa4424@gmail.com>
2026-06-10 13:41:50 +0530

    Implemented oops and inheritance

---
+++ oops_.py
@@ -0,0 +1,31 @@
+class Pop:
+  def sound(self,sound_):
+    self.sound_=sound_
+    print(f"The Sound is {self.sound_}")
+  def song(self,song_):
+    self.song_=song_
+    print(f"My fav Song is {self.song_}")
+  def album(self,al_name):
+    self.al_name=al_name
+    print(f"Bilie jean is from {self.al_name} album")
+  def artist(self,artist_name):
+    self.artist_name=artist_name
+    print(f"{artist_name} is so popular for his Moonwalk")
+obj=Pop()
+obj.sound("HeHe")
+obj.song("billieJean")
+obj.album("Thriller")
+obj.artist("MJ")
+
+
+
+
+
+
+
+
+  # sound="Hehe"
+  # album="Thriller"
+  # song="billiejean"
+  # artist:"MJ"
+  
