e1b5c925db687a13405ea516ec692816e5d77c3f
Sasi Priyaa <sasipriyaa4424@gmail.com>
2026-06-24 08:50:33 +0530

    complete implementation of tree datastructure

--- trees.py
+++ trees.py
@@ -1,75 +1,99 @@
-# class TreeNode:
-#     def __init__(self,val):
-#         self.val = val
-#         self.left = None
-#         self.right = None
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
-# def preorder(root):
-#     if root != None:
-#         print(root.val, end=" ")
-#         preorder(root.left)
-#         preorder(root.right)
-
-# def inorder(root):
-#     if root != None:
-#         inorder(root.left)
-#         print(root.val, end=" ")
-#         inorder(root.right)
-
-# def postorder(root):
-#     if root != None:
-#         postorder(root.left)
-#         postorder(root.right)
-#         print(root.val, end=" ")
-
-# root = TreeNode(1)
-# root.left = TreeNode(2)
-# root.right = TreeNode(3)
-# root.left.left = TreeNode(4)
-# root.left.right = TreeNode(5)
-# root.right.left = TreeNode(6)
-# root.right.right = TreeNode(7)
-
-# preorder(root)
-# print()
-# inorder(root)
-# print()
-# postorder(root)
-# print()
-
-class TreeNode:
-    def __init__(self,val):
-        self.val=val
-        self.left=None
-        self.right=None
-class BST:
+class Bst:
     def __init__(self):
-        self.root=None
-    def insert(self,val):
-        newnode=TreeNode(val)
-        if self.root == None:
-            self.root=newnode
-        def insertrec(root):
-            if val<root.val:
-                if root.left == None:
-                    root.left=newnode
-                else:
-                    insertrec(root.left)
-            elif val>root.val:
-                if root.right == None:
-                    root.right=newnode
-                else:
-                    insertrec(root.right)
-        insertrec(self.root)
+        self.root = None
 
-    def inorder(self,root):
+    def insert(self, val):
+        newnode = Node(val)
+
+        if self.root is None:
+            self.root = newnode
+            return
+
+        def insertat(root):
+            if val < root.val:
+                if root.left is None:
+                    root.left = newnode
+                else:
+                    insertat(root.left)
+
+            elif val > root.val:
+                if root.right is None:
+                    root.right = newnode
+                else:
+                    insertat(root.right)
+
+        insertat(self.root)
+
+    def inorder(self, root):
         if root:
             self.inorder(root.left)
-            print(root.val,end=" ")
+            print(root.val, end=" ")
             self.inorder(root.right)
-nodes = [18,24,15,55,4,39,17,23]
-bst = BST()
+
+    def search(self, t, root):
+        if root is None:
+            return False
+
+        if root.val == t:
+            return True
+
+        elif t < root.val:
+            return self.search(t, root.left)
+
+        else:
+            return self.search(t, root.right)
+
+    def findMin(self, node):
+        while node.left:
+            node = node.left
+        return node
+
+    def deleteNode(self, root, key):
+        if root is None:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            # Node with one child or no child
+            if root.left is None:
+                return root.right
+
+            if root.right is None:
+                return root.left
+
+            # Node with two children
+            successor = self.findMin(root.right)
+            root.val = successor.val
+            root.right = self.deleteNode(root.right, successor.val)
+
+        return root
+
+
+# Driver code
+nodes = [10, 7, 18, 29, 25, 24]
+
+bst = Bst()
+
 for i in nodes:
     bst.insert(i)
+
+print("Before deletion:")
 bst.inorder(bst.root)
-print()
+
+bst.root = bst.deleteNode(bst.root, 18)
+
+print("\nAfter deletion:")
+bst.inorder(bst.root)
+
+print("\nSearch 24:", bst.search(24, bst.root))
